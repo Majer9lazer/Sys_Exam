f3ab4651662d56022fe6f2c2a6b00de7163b85fe
СидоренкоЕ <@403_2>
2018-04-05 15:00:43 +0600

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemProgramming_Exam;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+namespace Consumer
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                if (args.Any())
+                {
+
+                    GetSmsFromRabbitMq q = new GetSmsFromRabbitMq();
+                    q.RunWorkerProcessForSmss(args[0]);
+                }
+
+                else
+                {
+                    Console.WriteLine("В аргументы ничего не пришло");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class GetSmsFromRabbitMq
+    {
+        public static IConnectionFactory _connectionFactory;
+
+
+
+        public void RunWorkerProcessForSmss(string queueName = "smss_to_send")
+        {
+            using (var connection = _connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName,
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                Console.WriteLine(" [*] Opened Channel");
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                Console.WriteLine($" [*] Waiting for messages from queue {queueName}");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    
+                  
+                    User messageDeserialized = JsonConvert.DeserializeObject<User>(message);
+                    using (StreamWriter sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "Hello.txt"))
+                    {
+                        sw.Write(message);
+                    MainWindow.SetOtherNumber(messageDeserialized.UserNumber);
+                    }
+                    Console.WriteLine($" [x] Deserialized object {messageDeserialized}");
+
+                    Console.WriteLine($" [x] Received {message}");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($" [x] Operation Completed {message}");
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                channel.BasicConsume(queue: queueName,
+                    autoAck: false,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        
+
+        public GetSmsFromRabbitMq()
+        {
+            _connectionFactory = new ConnectionFactory()
+            {
+                HostName = "192.168.111.199",
+                UserName = "shag",
+                Password = "shag",
+                VirtualHost = "/"
+            };
+        }
+    }
+}
---
+++ SystemProgramming_Exam/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using SystemProgramming_Exam.Logic;
+
+using Newtonsoft.Json;
+
+namespace SystemProgramming_Exam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        private RabbitMqMiddlewareBusService middleware = new RabbitMqMiddlewareBusService();
+
+        User u = new User();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //DispatcherTimer t = new DispatcherTimer();
+            //t.Interval = TimeSpan.FromSeconds(5);
+            //t.Tick += (a, b) =>
+            //{
+
+            //};
+        }
+
+        private int GeneRateRandomNumber()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 5);
+        }
+
+
+        public static void SayHello()
+        {
+            MessageBox.Show("Oh My Gad it is Working");
+        }
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            CurrentRandomNumberTextBlock.Text = null;
+            try
+            {
+                PartnerRandomNumberTextBlock.Text += staticPartnterTextBlockText;
+                int randNumber = GeneRateRandomNumber();
+                CurrentRandomNumberTextBlock.Text += randNumber;
+                if (!string.IsNullOrEmpty(CurrentRandomNumberTextBlock.Text))
+                {
+                    u.UserLuckyNumber = (CurrentRandomNumberTextBlock.Text);
+                    u.UserNumber = CurrentNumberTextBlock.Text;
+                    Process.Start("Consumer.exe", u.UserLuckyNumber.ToString());
+
+                }
+                else
+                {
+                    MessageBox.Show("Your random Number Is Empty!");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SeachSomeBody(object sender, RoutedEventArgs e)
+        {
+            u.UserLuckyNumber = (CurrentRandomNumberTextBlock.Text);
+            u.UserNumber = CurrentNumberTextBlock.Text;
+            middleware.PublishMessage(u, u.UserLuckyNumber);
+        }
+
+        public static string staticPartnterTextBlockText;
+        public static void SetOtherNumber(string number)
+        {
+            MessageBox.Show($"Number is - {number}");
+            staticPartnterTextBlockText += number;
+        }
+    }
+    public class User
+    {
+        public string UserLuckyNumber { get; set; }
+        public string UserNumber { get; set; }
+
+    }
+}
